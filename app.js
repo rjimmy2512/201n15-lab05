127a6fd180808bddbf785f07efaa93174332e627
R.James <rjimmy2512@gmail.com>
2019-08-21 21:01:09 -0700

    deleted 201n15-lab05

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { 
-  var totalProduct = a * b;
-  var string = 'The product of ' + a + ' and ' + b + ' is ' +totalProduct + '.';
-  return [totalProduct, string];
-}
+// function multiply(a, b) { 
+//   var totalProduct = a * b;
+//   var string = 'The product of ' + a + ' and ' + b + ' is ' +totalProduct + '.';
+//   return [totalProduct, string];
+// }
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// // Here is the test for multiply(); uncomment it to run it
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -43,12 +43,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { 
+  var totalSum = a + b + c;
+  var totalProduct = a * b * c;
+  var string1= '' + a +' and ' + b + ' and ' + c +' sum to ' + totalSum + '.';
+  var string2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + totalProduct + '.';
+  return[totalSum, totalProduct, string1, string2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
